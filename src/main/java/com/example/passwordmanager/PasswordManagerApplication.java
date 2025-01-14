f21f786dfec3570d29cef2efc4d514f8bb460a72
SahisN <sahisneupane23@gmail.com>
2025-01-14 00:02:41 -0800

    added login ui

--- src/main/java/com/example/passwordmanager/PasswordManagerApplication.java
+++ src/main/java/com/example/passwordmanager/PasswordManagerApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class PasswordManagerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PasswordManagerApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 550, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(PasswordManagerApplication.class.getResource("signup.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 560);
         stage.setTitle("Password Manager");
         stage.setResizable(false);
         stage.setScene(scene);
